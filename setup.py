f2321fb80bf1c66aaa809921408daf5a2230f748
Sorokovikov <themilkymouse@gmail.com>
2020-09-23 16:06:20 +0800

    Fix setup.py, README.md

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
-from os.path import join, dirname
+
+with open('README.md', mode='r', encoding='utf-8') as readme:
+    LONG_DESCRIPTION = readme.read()
 
 setup(
     name='time_series2complex_networks',
     version='0.1',
     packages=find_packages(),
-    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
     install_requires=[
         'matplotlib==3.3.0',
         'pandas==1.0.5',
